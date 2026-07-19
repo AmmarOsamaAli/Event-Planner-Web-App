30548010adb55c538633f31493adb33da7880246
AmmarOsamaAli <ammarosama080@icloud.com>
2026-07-19 10:53:31 +0300

    Created Attendence Request Functionality

--- controllers/attendance-request.controllers.js
+++ controllers/attendance-request.controllers.js
@@ -1,7 +1,25 @@
 const router = require("express").Router()
+const Event = require("../models/Event")
+const ParticipationRequest = require("../models/ParticipationRequest")
+const isSignedIn = require("../middleware/is-signed-in");
 
 
 
+router.get('/events/:eventId/attendance-requests/new', isSignedIn, async (req,res)=>{
+    const foundEvent = await Event.findById(req.params.eventId)
+    res.render('attendance-requests/new.ejs', {events: foundEvent})
+})
+
+router.post('/events/:eventId/attendance-requests', isSignedIn, async (req,res)=>{
+    const createRequest = ParticipationRequest.create({
+        message: req.body.message,
+        status: "pending",
+        type: "attendanceRequest",
+        participant: req.session.user._id,
+        event: req.params.eventId
+    })
+    res.redirect('/events')
+})
 
 
 module.exports = router;
