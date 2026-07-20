1e86ca604fafa6c14789b82344df538d264bf8ff
AmmarOsamaAli <ammarosama080@icloud.com>
2026-07-20 14:31:53 +0300

    Created All Inviations Page, and Create Invitation Functionality and Page

--- controllers/invitation.controllers.js
+++ controllers/invitation.controllers.js
@@ -1,6 +1,45 @@
 const router = require("express").Router()
 const Event = require("../models/Event")
 const ParticipationRequest = require("../models/ParticipationRequest")
+const User = require('../models/User.js')
+const isSignedIn = require("../middleware/is-signed-in");
+
+router.get('/events/invitations', isSignedIn ,async (req,res)=>{
+    const allInvitations = await ParticipationRequest.find({type: "invitation"})
+    res.render('invitations/index.ejs', {invitations: allInvitations, user: req.session.user})
+})
+
+
+router.get('/events/:eventsId/invitations/new', isSignedIn ,async (req,res)=>{
+    const foundEvent = await Event.findById(req.params.eventsId)
+    if(!foundEvent){
+        return res.redirect('/events/invitations')
+    }
+
+    if(!foundEvent.eventPlanner.equals(req.session.user._id)){
+        return res.redirect(`/events/${foundEvent._id}`)
+    }
+
+    const excludedUsers = [
+        foundEvent.eventPlanner,
+        ...foundEvent.attendeesList
+    ]
+
+    const users = await User.find({_id: { $nin: excludedUsers }})
+
+    res.render('invitations/new.ejs', { events: foundEvent, users })
+})
+
+router.post('/events/:eventsId/invitations', isSignedIn, async (req,res)=>{
+    const foundInvitation = await ParticipationRequest.create({
+        message: req.body.message,
+        type: "invitation",
+        participant: req.body.participant,
+        event: req.params.eventsId,
+    })
+
+    res.redirect('/events/invitations')
+})
 
 
 module.exports = router;
