7d463844c1308fa95bae25d66eee0346abf6c118
AmmarOsamaAli <ammarosama080@icloud.com>
2026-07-22 11:32:38 +0300

    Completed Sign-up and Log-in Design, And added Error Alret from controller and EJS

--- controllers/auth.controllers.js
+++ controllers/auth.controllers.js
@@ -6,17 +6,17 @@ const bcrypt = require("bcrypt");
 
 // Sign up routes
 router.get("/sign-up", (req, res) => {
-  res.render("auth/sign-up.ejs");
+  res.render("auth/sign-up.ejs", {errorMessage: null});
 });
 
 router.post("/sign-up", async (req, res) => {
   const userInDatabase = await User.findOne({ username: req.body.username });
   if (userInDatabase) {
-    return res.send("Username already taken.");
+    return res.status(400).render("auth/sign-up.ejs", {errorMessage: "Username already taken."})
   }
 
   if (req.body.password !== req.body.confirmPassword) {
-    return res.send("Password and Confirm Password must match");
+    return res.status(400).render("auth/sign-up.ejs", {errorMessage: "Passwords do not match"})
   }
 
   const hashedPassword = bcrypt.hashSync(req.body.password, 10);
@@ -32,7 +32,7 @@ router.post("/sign-up", async (req, res) => {
 
 // Sign in routes
 router.get("/log-in", (req, res) => {
-  res.render("auth/log-in.ejs");
+  res.render("auth/log-in.ejs", {errorMessage: null});
 });
 
 
@@ -41,7 +41,7 @@ router.post("/log-in", async (req, res) => {
   // First, get the user from the database
   const userInDatabase = await User.findOne({ username: req.body.username });
   if (!userInDatabase) {
-    return res.send("Login failed. Please try again.");
+    return res.status(400).render("auth/log-in.ejs", {errorMessage: "Login failed. Please try again."})
   }
 
   // There is a user! Time to test their password with bcrypt
@@ -50,7 +50,7 @@ router.post("/log-in", async (req, res) => {
     userInDatabase.password
   );
   if (!validPassword) {
-    return res.send("Login failed. Please try again.");
+    return res.status(400).render("auth/log-in.ejs", {errorMessage: "Login failed. Please try again."})
   }
 
   // There is a user AND they had the correct password. Time to make a session!
